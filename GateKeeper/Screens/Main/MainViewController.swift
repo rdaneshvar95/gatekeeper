283c3529c20d76e2c927902e6932bd63d542cc2b
Ray <ray@Barkhas-MacBook-Air.local>
2022-07-01 04:22:14 +0530

    Add main screen

---
+++ GateKeeper/Screens/Main/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  GateKeeper
+//
+//  Created by Ray on 01/07/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func loadView() {
+        view = MainView(frame: .zero)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
